2c971a08975cf54aad106617b0c21c91c57d0283
akgjohansson <johansson.mcquack@gmail.com>
2017-08-23 14:43:29 +0200

    Fixade i Program.Main

--- Program.cs
+++ Program.cs
@@ -10,6 +10,9 @@ namespace SubscriptionProject
     {
         static void Main(string[] args)
         {
+            FrontEnd frontEnd = new FrontEnd();
+            string companyName = frontEnd.GetCompanyName();
+            Company companty = new Company(companyName);
         }
     }
 }
