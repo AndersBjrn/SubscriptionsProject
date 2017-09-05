e25dcdcd90cca20518f4f9abba5861eaf4c9d611
akgjohansson <johansson.mcquack@gmail.com>
2017-09-05 10:33:39 +0200

    Grundstomme för testning av att hämna artiklar samt skapa mail

---
+++ UnitTestProject1/GetArticlesAndSendMail.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class GetArticlesAndSendMail
+    {
+        [TestMethod]
+        public void TestGetUpdates()
+        {
+        }
+
+        [TestMethod]
+        public void TestGetArticles()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestCreateMail()
+        {
+
+        }
+
+        
+    }
+}
