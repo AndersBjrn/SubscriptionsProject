48b8bd524f27276aba0ae757dcbe9126d3c3f75c
akgjohansson <johansson.mcquack@gmail.com>
2017-09-05 11:16:35 +0200

    Satte upp mål för test

--- UnitTestProject1/GetArticlesAndSendMail.cs
+++ UnitTestProject1/GetArticlesAndSendMail.cs
@@ -9,17 +9,27 @@ namespace UnitTestProject1
         [TestMethod]
         public void TestGetUpdates()
         {
+            /* Mål för testet: Vi har en subscriber med en lista av kategorier. När vi 
+             * kallar på GetUpdates så ska vi få alla uppdateringar de senaste sju dagarn
+             * I framtiden kan vi lämna plats för möjligheten att specifiera tidsintervallet
+             * som vi vill ha uppdateringar från
+             */
         }
 
         [TestMethod]
         public void TestGetArticles()
         {
+            /* Denna funktion ska hämta hem alla artiklar från en kategori och dess underkategorier */
 
         }
 
         [TestMethod]
         public void TestCreateMail()
         {
+            /* Denna funktion ska, utifrån en subscriber:
+             * samla in artiklar som subscribern har efterfrågat,
+             * sätta samman ett mail som innehåller efterfrågad information, samt
+             * skicka ut mailet */
 
         }
 
