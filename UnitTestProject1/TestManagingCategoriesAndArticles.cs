c8a1fbf1d18ddaf28eb5b0fc1d437750643d8c4f
akgjohansson <johansson.mcquack@gmail.com>
2017-09-05 12:59:35 +0200

    Börjar på testning av att skapa company, kategorier och artiklar

--- UnitTestProject1/TestManagingCategoriesAndArticles.cs
+++ UnitTestProject1/TestManagingCategoriesAndArticles.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SubscriptionProject;
 
 namespace UnitTestProject1
 {
     class TestManagingCategoriesAndArticles
     {
+        public Company CreateCompanyCategoriesAndArticles()
+        {
+            Company company = new Company("Test Company");
+
+            return company;
+        }
     }
 }
