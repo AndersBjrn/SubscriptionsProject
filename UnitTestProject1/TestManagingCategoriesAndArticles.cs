d91ccdaa3a01a54f11b76029492bf1473aa0af4d
akgjohansson <johansson.mcquack@gmail.com>
2017-09-05 13:02:22 +0200

    Fortsatt arbete på testet av company, category och articles

--- UnitTestProject1/TestManagingCategoriesAndArticles.cs
+++ UnitTestProject1/TestManagingCategoriesAndArticles.cs
@@ -12,7 +12,16 @@ namespace UnitTestProject1
         public Company CreateCompanyCategoriesAndArticles()
         {
             Company company = new Company("Test Company");
+            company.AddCategory(company, "", "News");
+            company.AddCategory(company, "News", "Bad news");
+            company.AddCategory(company, "News", "Good news");
+            company.AddCategory(company, "", "About us");
+            company.AddCategory(company, "About us", "Careers");
+            company.AddCategory(company, "About us", "Environment");
 
+            Category aboutUs = company.SubscriptionableCategories["About us"];
+            Category career = company.SubscriptionableCategories["Careers"];
+            Category environment = company.SubscriptionableCategories["Environment"];
             return company;
         }
     }
