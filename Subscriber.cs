a70b9a604b5746f7ceb51f765748191b94d9a7d4
akgjohansson <johansson.mcquack@gmail.com>
2017-09-08 10:05:40 +0200

    Kommenterade bort CheckChildren

--- Subscriber.cs
+++ Subscriber.cs
@@ -72,21 +72,21 @@ namespace SubscriptionProject
             }
         }
 
-        void CheckChildren(Category cat)
-        {
-            if (Subscription.Contains(cat))
-            {
-                RemoveCategoryFromSubscription(cat);
-                return;
-            }
-            else if (cat.Subcategories != null)
-            {
-                foreach (Category category in cat.Subcategories)
-                {
-                    CheckChildren(category);
-                }
-            }
-        }
+        //void CheckChildren(Category cat)
+        //{
+        //    if (Subscription.Contains(cat))
+        //    {
+        //        RemoveCategoryFromSubscription(cat);
+        //        return;
+        //    }
+        //    else if (cat.Subcategories != null)
+        //    {
+        //        foreach (Category category in cat.Subcategories)
+        //        {
+        //            CheckChildren(category);
+        //        }
+        //    }
+        //}
 
         public void AddCategoryToSubscription(Category c)//Lägger till en ny kategori om inte den eller en förälder redan finns i listan
         {
